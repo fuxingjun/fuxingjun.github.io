66dd6c02f44193625d611898fbdce04f43cc54cd
Jaden Fu <1151265463@qq.com>
2018-04-06 00:48:47 +0800

    add some works
    加上部分作品

--- js/main.js
+++ js/main.js
@@ -10,3 +10,12 @@ $('nav ul li').click(function(){
 	mySwiper.slideTo($(this).index(),500,false);
 	$('.menu').click();
 });
+$('#works div.swiper-slide').children().hide();
+$('#works div.swiper-slide').hover(
+	function(){
+		$(this).children().fadeIn();
+	},
+	function(){
+		$(this).children().fadeOut();
+	}
+);
